18b58bef14d2e381632109d50e72f85931a9657b
yoiner_duran <duranyoiner86@gmail.com>
2023-03-14 16:11:14 -0500

    vistas creadas

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def home(request):
     context={'titulo':'Principal'}
-    return render(request, 'main.html',context)
+    return render(request, 'home.html',context)
